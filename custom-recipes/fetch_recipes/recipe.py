d2315753d05cfe7319b2614bf6522aba4e466dea
Harizo Rajaona <harizo.rajaona@gmail.com>
2019-02-18 10:53:08 +0100

    First version of recipe.py

--- custom-recipes/fetch_recipes/recipe.py
+++ custom-recipes/fetch_recipes/recipe.py
@@ -1,39 +1,57 @@
 
 import dataiku
+import logging
+import requests
+import json
+
 from dataiku.customrecipe import *
 
-output_names = get_output_names_for_role('output_role')
-output_datasets = [dataiku.Dataset(name) for name in output_names]
+BASE_URL = "https://www.food2fork.com/api/"
 
-my_variable = get_recipe_config()['parameter_name']
+logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s")
+logging.getLogger().setLevel(logging.INFO)
 
-# For optional parameters, you should provide a default value in case the parameter is not present:
-my_variable = get_recipe_config().get('parameter_name', None)
+logging.info("Fetching parameters...")
+recipe_config = get_recipe_config()
+nb_max_search = int(recipe_config.get("nb_max_search"))
+meal_type = recipe_config.get("meal_type")
 
-# Note about typing:
-# The configuration of the recipe is passed through a JSON object
-# As such, INT parameters of the recipe are received in the get_recipe_config() dict as a Python float.
-# If you absolutely require a Python int, use int(get_recipe_config()["my_int_param"])
+logging.info("Retrieving API key..")
+client = dataiku.api_client()
+# Leverage user secrets for properly accessing API key:
+auth_info = client.get_auth_info(with_secrets=True)
+api_key = None
+for secret in auth_info["secrets"]:
+    if secret["key"] == "food2fork":
+        api_key = secret["value"]
+        break
+if not secret_value:
+    raise Exception("Secret not found!")
 
+logging.info("Querying the SEARCH endpoint...")
+search_endpoint = BASE_URL + "search"
+params = {"key": api_key, "q": meal_type, "count": nb_max_search}
+req_search = requests.post(search_endpoint, data=params)
+search_output = json.loads(req_search.text)
 
-#############################
-# Your original recipe
-#############################
+logging.info("Processing and forwarding to the GET endpoint...")
+get_endpoint = BASE_URL + "get"
+final_outputs = [{"id": rcp["recipe_id"],
+                  "title": rcp["title"]} for rcp in search_output["recipes"]]
 
-# -*- coding: utf-8 -*-
-import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
+logging.info("Preparing output dataset...")
+output_dataset_name = get_output_names_for_role("output_role")
+output_dataset = dataiku.Dataset(output_dataset_name)
+output_schema = [{"name": "id", "type": "string"},
+                 {"name": "title", "type": "string"},
+                 {"name": "ingredients", "type": "string"}]
+output_dataset.write_schema(output_schema)
 
-
-
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-dummy_df = ... # Compute a Pandas dataframe to write into dummy
-
-
-# Write recipe outputs
-dummy = dataiku.Dataset("dummy")
-dummy.write_with_schema(dummy_df)
+logging.info("Writing results in output dataset...")
+writer = output_dataset.get_writer()
+for output in final_outputs:
+    req_ing = requests.post(get_endpoint, data={"key": api_key, "rId": output["id"]})
+    resp = json.loads(req_ing.text)
+    output["ingredients"] = resp["recipe"]["ingredients"]
+    writer.write_row_dict(output)
+writer.close()
