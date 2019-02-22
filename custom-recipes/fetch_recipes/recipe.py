0b597af3a6877bdc50187825efa8ff302c3e2fbc
admin <admin>
2019-02-22 09:56:09 +0000

    Edited file 'custom-recipes/fetch_recipes/recipe.py'

--- custom-recipes/fetch_recipes/recipe.py
+++ custom-recipes/fetch_recipes/recipe.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from dataiku.customrecipe import *
 
+# The base URL:
 BASE_URL = "https://www.food2fork.com/api/"
 
 logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s")
